610ee1faa476e3da8ce3cd90dcf1cbf671edae4f
sayeedhossain2 <sayeedh95@gmail.com>
2022-11-08 05:38:28 +0600

    banner section add

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import img1 from "../../../assets/banner/homebanner.jpg";
+
+const Banner = () => {
+  return (
+    <div>
+      <img className="w-full h-[89vh]" src={img1} alt="" />
+    </div>
+  );
+};
+
+export default Banner;
