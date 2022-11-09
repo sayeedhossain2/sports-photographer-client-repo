908950ea277759bbdffd49e5bf9086da9f8a4514
sayeedhossain2 <sayeedh95@gmail.com>
2022-11-09 11:14:05 +0600

    react spinner added

--- src/Router/PrivateRoute/PrivateRoute.js
+++ src/Router/PrivateRoute/PrivateRoute.js
@@ -1,13 +1,26 @@
 import React, { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { AuthContext } from "../../Context/AuthProvider/AuthProvider";
+// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import { Dna } from "react-loader-spinner";
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
   const location = useLocation();
 
   if (loading) {
-    return <h2>Loading....</h2>;
+    return (
+      <h2 className=" flex justify-center items-center">
+        <Dna
+          visible={true}
+          height="200"
+          width="280"
+          ariaLabel="dna-loading"
+          wrapperStyle={{}}
+          wrapperClass="dna-wrapper"
+        />
+      </h2>
+    );
   }
 
   if (user) {
