5a20efb98addcb71cf64abcd666fb9d11d03b376
sayeedhossain2 <sayeedh95@gmail.com>
2022-11-10 14:12:03 +0600

    login registration edit

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -70,15 +70,8 @@ const SignUp = () => {
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row">
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Sign Up</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-        </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <h1 className="text-5xl font-bold mt-5">Sign Up</h1>
           <form onSubmit={handleUserSignUp} className="card-body">
             <div className="form-control">
               <label className="label">
